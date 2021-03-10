55b2e6b8c208a3dada7711c383a9e74eee5b101f
monp1202 <penelopemontminy@gmail.com>
2021-03-10 13:16:08 -0500

    Ajout de commentaires

--- Code/API/Python/tests/test_vecteur.py
+++ Code/API/Python/tests/test_vecteur.py
@@ -10,14 +10,17 @@ class TestVecteur(unittest.TestCase):
         self.assertTrue(repr(VecteurTest)== "(3;-2;7)",msg=f"__repr__() donne: {VecteurTest.__repr__()}")
     
     def test_add(self):
+        """Test d'addition de deux vecteurs"""
         VecteurBase = Vecteur(2,7,-4)
         VecteurAdd = Vecteur(-7,2,10)
         self.assertTrue(VecteurBase + VecteurAdd == Vecteur(-5,9,6), msg=f"__add__() donne: {VecteurBase + VecteurAdd}")
 
     def test_add_null(self):
+        """Test d'addition d'un vecteur null avec un vecteur ayant des valeurs"""
         self.assertTrue(Vecteur() + Vecteur(2,5,8.5) == Vecteur(2,5,8.5),  msg=f"__add__() donne: {Vecteur() + Vecteur(2,5,8.5)}")
     
     def test_add_err(self):
+        """Test d'addition d'un nombre réel à un vecteur"""
         VecteurBase = Vecteur(5,-2,13)
         Reel = 8
         with self.assertRaises(TypeError, msg = f"Aucune erreur n'a été trouvé"):
