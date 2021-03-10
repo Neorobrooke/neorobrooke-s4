a612dd5fd840dcfbc6b080818d9dd4abaa4e1a25
Camille Durivage Guertin <55754922+mrscamille@users.noreply.github.com>
2021-03-10 13:11:48 -0500

    Test direction, autres variables

--- Code/API/Python/tests/test_direction.py
+++ Code/API/Python/tests/test_direction.py
@@ -4,16 +4,20 @@ from funibot_api.funibot import Direction
 
 class TestsDirection(unittest.TestCase):
 
-    def test_repr_Cas1(self):
-        """ Cas fonctionnel, impose une valeur logique chaque variable"""
+    def test_repr_Normal(self):
+        """ Impose une valeur logique chaque variable """
         directionCas1 = Direction("4x-8y+z")
         self.assertTrue(repr(directionCas1) == "Direction(x:4.0; y:-8.0; z:1.0)", msg=f"__repr__() donne: {repr(directionCas1)}")
     
-    def test_repr_Cas2(self):
-        """ Cas non fonctionnel, possede une valeur qui a un +-"""
+    def test_repr_DoubleSigne(self):
+        """ Double signe """
         
         with self.assertRaises(ValueError, msg="Aucune erreur pour un double signe"):
             Direction("4x+-8y+z")
     
-    
+    def test_repr_Variables(self):
+        """ Autres caracteres que xyz """
+        
+        with self.assertRaises(ValueError, msg="Aucune erreur pour un autre caractères que xyz"):
+            Direction("4a-8y+z")
 
