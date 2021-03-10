e923d37ae4428cd2a119b390f169a05db67cc548
Camille Durivage Guertin <55754922+mrscamille@users.noreply.github.com>
2021-03-10 13:04:12 -0500

    Test direction double signe

--- Code/API/Python/tests/test_direction.py
+++ Code/API/Python/tests/test_direction.py
@@ -15,15 +15,5 @@ class TestsDirection(unittest.TestCase):
         with self.assertRaises(ValueError, msg="Aucune erreur pour un double signe"):
             Direction("4x+-8y+z")
     
-    """def test_repr_Cas3(self):
-        " Cas non fonctionnel, possede pas de x"
-        directionCas3 = Direction("4+8y+z")
-        self.assertTrue(repr(directionCas3) == "Direction(x:4.0; y:-8.0; z:1.0)", msg=f"__repr__() donne: {repr(directionCas3)}")
     
-     def test_repr_Cas4(self):
-        " Cas non fonctionnel, possede une valeur qui a un +-"
-        directionCas4 = Direction("4-8+2")
-        self.assertTrue(repr(directionCas4) == "Direction(x:4.0; y:-8.0; z:1.0)", msg=f"__repr__() donne: {repr(directionCas4)}")
-    """
-
 
