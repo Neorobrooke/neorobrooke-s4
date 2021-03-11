aa3666b093342985534200356268c177488f4b63
monp1202 <penelopemontminy@gmail.com>
2021-03-11 17:24:46 -0500

    Test de soustraction normale et avec vecteur null

--- Code/API/Python/tests/test_vecteur.py
+++ Code/API/Python/tests/test_vecteur.py
@@ -25,3 +25,14 @@ class TestVecteur(unittest.TestCase):
         Reel = 8
         with self.assertRaises(TypeError, msg = f"Aucune erreur n'a été trouvé"):
             VecteurBase + Reel 
+
+    def test_sub(self):
+        """ Test soustraction de deux vecteurs"""
+        VecteurBase = Vecteur(2,7,-4)
+        VecteurAdd = Vecteur(-7,2,10)
+        self.assertTrue(VecteurBase - VecteurAdd == Vecteur(9,5,-14), msg=f"__add__() donne: {VecteurBase - VecteurAdd}")
+
+    def test_sub_null(self):
+        """ Soustraction par un vecteur null"""
+        self.assertTrue(Vecteur(2,5,8.5) - Vecteur() == Vecteur(2,5,8.5),  msg=f"__add__() donne: {Vecteur(2,5,8.5)-Vecteur()}")
+    
