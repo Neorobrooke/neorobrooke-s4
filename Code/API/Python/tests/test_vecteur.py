d2b3a0219060a7ecdb7c6c03ceefaa055f7400d8
monp1202 <penelopemontminy@gmail.com>
2021-03-11 17:38:32 -0500

    Tests de multiplication

--- Code/API/Python/tests/test_vecteur.py
+++ Code/API/Python/tests/test_vecteur.py
@@ -29,12 +29,12 @@ class TestVecteur(unittest.TestCase):
     def test_sub(self):
         """ Test soustraction de deux vecteurs"""
         VecteurBase = Vecteur(2,7,-4)
-        VecteurAdd = Vecteur(-7,2,10)
-        self.assertTrue(VecteurBase - VecteurAdd == Vecteur(9,5,-14), msg=f"__add__() donne: {VecteurBase - VecteurAdd}")
+        VecteurSub = Vecteur(-7,2,10)
+        self.assertTrue(VecteurBase - VecteurSub == Vecteur(9,5,-14), msg=f"__sub__() donne: {VecteurBase - VecteurSub}")
 
     def test_sub_null(self):
         """ Soustraction par un vecteur null"""
-        self.assertTrue(Vecteur(2,5,8.5) - Vecteur() == Vecteur(2,5,8.5),  msg=f"__add__() donne: {Vecteur(2,5,8.5)-Vecteur()}")
+        self.assertTrue(Vecteur(2,5,8.5) - Vecteur() == Vecteur(2,5,8.5),  msg=f"__sub__() donne: {Vecteur(2,5,8.5)-Vecteur()}")
     
     def test_sub_err(self):
         """ Test de soustraction d'un nombre réel à un vecteur"""
@@ -42,3 +42,20 @@ class TestVecteur(unittest.TestCase):
         Reel = 8
         with self.assertRaises(TypeError, msg = f"Aucune erreur n'a été trouvé"):
             VecteurBase - Reel
+
+    def test_mul_err(self):
+        """ Test multiplication de deux vecteurs"""
+        VecteurBase = Vecteur(2,7,-4)
+        VecteurMul = Vecteur(-7,2,10)
+        with self.assertRaises(TypeError, msg = f"Aucune erreur n'a été trouvé"):
+            VecteurBase * VecteurMul
+
+    def test_mul_null(self):
+        """ Test de multiplication par zéro"""
+        self.assertTrue(Vecteur(2,5,8.5) * 0 == Vecteur(),  msg=f"__mul__() donne: {Vecteur(2,5,8.5)*0}")
+    
+    def test_mul(self):
+        """ Test de multiplication d'un nombre réel à un vecteur"""
+        VecteurBase = Vecteur(5,-2,13)
+        Reel = 8
+        self.assertTrue(VecteurBase * Reel == Vecteur(40,-16,104), msg=f"__mul__() donne: {VecteurBase * Reel}")
