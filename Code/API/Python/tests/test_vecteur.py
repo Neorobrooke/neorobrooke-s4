cefc995af9c421708c69404473c15ef74696fa6e
monp1202 <penelopemontminy@gmail.com>
2021-03-13 12:30:16 -0500

    ajout test négatif pour mul et div

--- Code/API/Python/tests/test_vecteur.py
+++ Code/API/Python/tests/test_vecteur.py
@@ -8,7 +8,7 @@ class TestVecteur(unittest.TestCase):
     def test_repr(self):
         """Test d'un vecteur pour voir s'il se représente bien"""
         vecteur_test = Vecteur(3,-2,7)
-        self.assertTrue(repr(vecteur_test)== "(3;-2;7)",msg=f"__repr__() donne: {vecteur_test.__repr__()}")
+        self.assertTrue(repr(vecteur_test) == "(3;-2;7)",msg = f"__repr__() donne: {vecteur_test.__repr__()}")
     
     def test_add(self):
         """Test d'addition de deux vecteurs"""
@@ -21,24 +21,24 @@ class TestVecteur(unittest.TestCase):
     def test_add_entier(self):
         """Test d'addition d'un nombre entier à un vecteur"""
         vecteur_base = Vecteur(5,-2,13)
-        scalair_entier = 8
+        scalaire_entier = 8
         with self.assertRaises(TypeError, msg = f"L'addition avec autre chose qu'un vecteur (comme un scalaire) n'a pas levé d'exception de type TypeError"):
-            vecteur_base + scalair_entier 
+            vecteur_base + scalaire_entier 
 
     def test_sub(self):
         """Test soustraction de deux vecteurs"""
-        self.assertTrue(Vecteur(2,7,-4) - Vecteur(-7,2,10) == Vecteur(9,5,-14), msg=f"Vecteur(2,7,-4) - Vecteur(-7,2,10) donne: {Vecteur(2,7,-4) - Vecteur(-7,2,10)}")
+        self.assertTrue(Vecteur(2,7,-4) - Vecteur(-7,2,10) == Vecteur(9,5,-14), msg = f"Vecteur(2,7,-4) - Vecteur(-7,2,10) donne: {Vecteur(2,7,-4) - Vecteur(-7,2,10)}")
 
     def test_sub_nul(self):
         """Soustraction par un vecteur nul"""
-        self.assertTrue(Vecteur(2,5,8.5) - Vecteur() == Vecteur(2,5,8.5),  msg=f"Vecteur(2,5,8.5)-Vecteur() donne: {Vecteur(2,5,8.5)-Vecteur()}")
+        self.assertTrue(Vecteur(2,5,8.5) - Vecteur() == Vecteur(2,5,8.5),  msg = f"Vecteur(2,5,8.5) - Vecteur() donne: {Vecteur(2,5,8.5) - Vecteur()}")
     
     def test_sub_entier(self):
         """Test de soustraction d'un nombre entier à un vecteur"""
         vecteur_base = Vecteur(5,-2,13)
-        scalair_entier = 8
-        with self.assertRaises(TypeError, msg = f"La soustraction avec autre chose qu'un vecteur (comme un nombre réel) n'a pas levé d'exception de type TypeError"):
-            vecteur_base - scalair_entier
+        scalaire_entier = 8
+        with self.assertRaises(TypeError, msg = f"La soustraction avec autre chose qu'un vecteur (comme un scalaire) n'a pas levé d'exception de type TypeError"):
+            vecteur_base - scalaire_entier
 
     def test_mul_vecteur(self):
         """Test multiplication de deux vecteurs"""
@@ -47,19 +47,25 @@ class TestVecteur(unittest.TestCase):
 
     def test_mul_zero(self):
         """Test de multiplication par zéro"""
-        self.assertTrue(Vecteur(2,5,8.5) * 0 == Vecteur(),  msg=f"Vecteur(2,5,8.5)*0 donne: {Vecteur(2,5,8.5)*0}")
+        self.assertTrue(Vecteur(2,5,8.5) * 0 == Vecteur(),  msg = f"Vecteur(2,5,8.5) * 0 donne: {Vecteur(2,5,8.5) * 0}")
     
     def test_mul_entier(self):
         """Test de multiplication d'un nombre entier à un vecteur"""
         vecteur_base = Vecteur(5,-2,13)
-        scalair_entier = 8
-        self.assertTrue(vecteur_base * scalair_entier == Vecteur(40,-16,104), msg=f"Vecteur(5,-2,13) * 8 donne: {vecteur_base * scalair_entier}")
+        scalaire_entier = 8
+        self.assertTrue(vecteur_base * scalaire_entier == Vecteur(40,-16,104), msg = f"Vecteur(5,-2,13) * 8 donne: {vecteur_base * scalaire_entier}")
 
     def test_mul_reel(self):
-        """Test de multiplication d'un nombre entier à un vecteur"""
+        """Test de multiplication d'un nombre réel à un vecteur"""
         vecteur_base = Vecteur(5,-2,13)
-        scalair_reel = 8.45
-        self.assertTrue(vecteur_base * scalair_reel == Vecteur(42.25,-16.9,109.85), msg=f"Vecteur(5,-2,13) * 8.45 donne: {vecteur_base * scalair_reel}")
+        scalaire_reel = 8.45
+        self.assertTrue(vecteur_base * scalaire_reel == Vecteur(42.25,-16.9,109.85), msg = f"Vecteur(5,-2,13) * 8.45 donne: {vecteur_base * scalaire_reel}")
+    
+    def test_mul_entier_negatif(self):
+        """Test de multiplication d'un nombre entier négatif à un vecteur"""
+        vecteur_base = Vecteur(5,-2,13)
+        scalaire_entier_negatif = -8
+        self.assertTrue(vecteur_base * scalaire_entier_negatif == Vecteur(-40,16,-104), msg = f"Vecteur(5,-2,13) * -8 donne: {vecteur_base * scalaire_entier_negatif}")
 
     def test_truediv_vecteur(self):
         """Test division de deux vecteurs"""
@@ -73,11 +79,22 @@ class TestVecteur(unittest.TestCase):
 
     def test_truediv_nul_par_entier(self):
         """Test divsion du vecteur nul par un entier"""
-        self.assertTrue(Vecteur() / 9 == Vecteur(),  msg=f"Vecteur() / 9 donne: {Vecteur()/9}")
+        self.assertTrue(Vecteur() / 9 == Vecteur(),  msg = f"Vecteur() / 9 donne: {Vecteur() / 9}")
     
     def test_truediv_entier(self):
         """Test de division d'un vecteur par un nombre entier"""
         vecteur_base = Vecteur(5,-2,13)
-        scalair_entier = 8
-        self.assertTrue(vecteur_base / scalair_entier == Vecteur(5/8,-1/4,13/8), msg=f"Vecteur(5,-2,13) / 8 donne: {vecteur_base / scalair_entier}")
-        
+        scalaire_entier = 8
+        self.assertTrue(vecteur_base / scalaire_entier == Vecteur(5/8,-1/4,13/8), msg = f"Vecteur(5,-2,13) / 8 donne: {vecteur_base / scalaire_entier}")
+
+    def test_truediv_reel(self):
+        """Test de division d'un vecteur par un nombre réel"""
+        vecteur_base = Vecteur(5,-2,13)
+        scalaire_reel = 2.5
+        self.assertTrue(vecteur_base / scalaire_reel == Vecteur(2,-0.8,5.2), msg = f"Vecteur(5,-2,13) / 2.5 donne: {vecteur_base / scalaire_reel}")
+
+    def test_truediv_entier_negatif(self):
+        """Test de division d'un vecteur par un nombre entier négatif"""
+        vecteur_base = Vecteur(5,-2,13)
+        scalaire_entier_negatif = -8
+        self.assertTrue(vecteur_base / scalaire_entier_negatif == Vecteur(-5/8,1/4,-13/8), msg = f"Vecteur(5,-2,13) / -8 donne: {vecteur_base / scalaire_entier_negatif}")
