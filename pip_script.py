caeeaa344a8dc5b8a43263981941049e4b754367
vililichi <keven0230@live.ca>
2021-03-13 12:43:07 -0500

    Ajout du pip_script de api-python

--- pip_script.py
+++ pip_script.py
@@ -1,13 +1,17 @@
-from sys import platform
+from sys import platform, executable
 import pathlib
 import os
 
+python_exec = pathlib.Path(executable)
+python_exec = f"{python_exec.stem}{python_exec.suffix}"
 
 if not pathlib.Path(".venv").exists():
-    os.system("python -m pip install -U pip")
-    os.system("python -m venv .venv")
+    os.system(f"{python_exec} -m pip install -U pip")
+    os.system(f"{python_exec} -m venv .venv")
 
 if platform == 'win32':
-    os.system("\"%cd%/.venv/Scripts/activate.bat\" && python -m pip install -r requirements.txt")
+    python_venv_exec = str(pathlib.Path(f".venv/Scripts/{python_exec}"))
 else:
-    os.system("source .venv/Scripts/activate && python -m pip install -r requirements.txt")
+    python_venv_exec = str(pathlib.Path(f".venv/bin/{python_exec}"))
+
+os.system(f"{python_venv_exec} -m pip install -r requirements.txt")
