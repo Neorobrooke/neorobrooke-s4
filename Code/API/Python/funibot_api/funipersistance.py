c1f79ad45a4d44738b602bb74fadb7b5da35e65c
philippewarren <philippewarren31@gmail.com>
2021-03-24 15:26:25 -0400

    Ajout de fonctions pour la persistance

--- Code/API/Python/funibot_api/funipersistance.py
+++ Code/API/Python/funibot_api/funipersistance.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
-from typing import Union
+from typing import Union, Tuple, List, Optional
 from pathlib import Path
+from numbers import Real
 
 from ruamel.yaml import YAML
 from typing import OrderedDict
@@ -13,8 +14,25 @@ class FuniPersistance:
         self.yaml.explicit_start = True
         self.yaml.explicit_end = True
 
-    def generer_yaml(self, donnees: dict):
-        pass
+    def maj(self, poteaux: List[dict], longueurs: List[Real]):
+        for data, long in zip(poteaux, longueurs):
+            cle = list(data.keys())[0]
+            data[cle]["cable"] = long
+
+            self.fichier["poteaux"][cle] = data[cle]
+
+    def calibrer(self, poteaux: List[dict]) -> Optional[List[Real]]:
+        longueurs = []
+        for pot in poteaux:
+            cle = list(pot.keys())[0]
+            if cle not in self.fichier["poteaux"].keys():
+                return None
+            if pot[cle]["poles"] != self.fichier["poteaux"][cle]["poles"]:
+                return None
+            if pot[cle]["accroches"] != self.fichier["poteaux"][cle]["accroches"]:
+                return None
+            longueurs.append(self.fichier["poteaux"][cle]["cable"])
+        return longueurs
 
     def _dump(self) -> None:
         with self.nom_fichier.open('w') as f:
