4c722396f7066a72adc1ef778c7387e14b247b9c
monp1202 <penelopemontminy@gmail.com>
2021-03-30 11:54:08 -0400

    Commencement test funiserial, setup

--- Code/API/Python/tests/test_funiserial.py
+++ Code/API/Python/tests/test_funiserial.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from funibot_api.funiserial import FuniErreur, FuniSerial, eFuniErreur
+from tests.mock_serial import MockSerial, MockType, DualMockSerial
 import unittest
 
 class TestFuniErreur(unittest.TestCase):
@@ -14,3 +15,16 @@ class TestFuniErreur(unittest.TestCase):
 
     def Representation(self):
         pass
+
+class TestsFuniSerial(unittest.TestCase):
+    """Test sur la classe FuniSerial"""
+
+    def SetUp(self):
+        """Initialisation commune à plusieurs tests"""
+        self.mock = MockSerial(MockType.TEST)
+        self.emock = MockSerial(MockType.TEST)
+        self.dmock = DualMockSerial(canal_lecture=self.mock, canal_ecriture=self.emock)
+
+    #def test_init(self):
+     #   """Test d'initialisation d'un FuniSerial"""
+      #  bot = FuniSerial(self.mock)
