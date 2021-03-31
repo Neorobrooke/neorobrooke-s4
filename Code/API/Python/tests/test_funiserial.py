6d70938676b05f8963d315c58a994fdf358cadde
monp1202 <penelopemontminy@gmail.com>
2021-03-31 11:03:32 -0400

    Tests pot erreur position none et ack au lieu de set

--- Code/API/Python/tests/test_funiserial.py
+++ Code/API/Python/tests/test_funiserial.py
@@ -63,3 +63,24 @@ class TestsFuniSerial(unittest.TestCase):
             bot.pot(FuniType.SET,-2,position)
         self.assertEqual(str(re.exception), "id est inférieur à 0")
 
+    def test_pot_err_pos_none(self):
+        """Test du poteau du FuniSerial avec une position None"""
+        bot = FuniSerial(self.mock)
+        position = None
+
+        with self.assertRaises(ValueError, msg = "La présence de None comme position n'a pas levé d'exception de type ValueError") as re:
+            bot.pot(FuniType.SET,2,position)
+        self.assertEqual(str(re.exception), "position est None")
+
+    def test_pot_ack(self):
+        """Test du poteau du FuniSerial avec ack"""
+        bot = FuniSerial(self.mock)
+        position = (3,1.2,8)
+        bot.pot(FuniType.GET,0,position)
+
+        validation_requete = bytes(
+            f'{{"comm": "pot", "type": "get", "args": {{"id": 0, "pos_x": null, "pos_y": null, "pos_z": null}}}}', 'utf8')
+
+        self.assertEqual(self.mock.requete, validation_requete,
+                         msg=f"Le poteau est {validation_requete} au lieu de {self.mock.requete}")
+
