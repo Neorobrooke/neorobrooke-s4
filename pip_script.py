c762df7726b1ce2bb852cc81656d721d0a5969e9
philippewarren <philippewarren31@gmail.com>
2021-04-01 12:58:32 -0400

    Ajout d'un argument path à pip_script

--- pip_script.py
+++ pip_script.py
@@ -1,5 +1,5 @@
 from sys import platform, executable
-import pathlib
+from pathlib import Path
 import os
 import argparse
 
@@ -9,10 +9,12 @@ if __name__ == '__main__':
     parser.add_argument("--noedit", action='store_true', help="Installer en mode éditable")
     parser.add_argument("--nodev", action='store_true', help="Installer sans les dépendances de développement")
     parser.add_argument("--notest", action='store_true', help="Installer sans les dépendances de test")
+    parser.add_argument("--path", type=Path, help="Chemin de la librairie")
     args = parser.parse_args()
     editable = "-e" if not args.noedit else ""
     test = "test" if not args.notest else ""
     dev = "dev" if not args.nodev else ""
+    path = args.path if args.path is not None else "."
 
     arg_extras = "[" if test != "" or dev != "" else ""
     arg_extras += test
@@ -20,18 +22,18 @@ if __name__ == '__main__':
     arg_extras += dev
     arg_extras += "]" if test != "" or dev != "" else ""
     
-    python_exec = pathlib.Path(executable)
+    python_exec = Path(executable)
     python_exec = f"{python_exec.stem}{python_exec.suffix}"
 
-    if not pathlib.Path(".venv").exists():
+    if not Path(".venv").exists():
         os.system(f"{python_exec} -m pip install -U pip setuptools")
         os.system(f"{python_exec} -m venv .venv")
 
     if platform == 'win32':
-        python_venv_exec = str(pathlib.Path(f".venv/Scripts/{python_exec}"))
+        python_venv_exec = str(Path(f".venv/Scripts/{python_exec}"))
     else:
-        python_venv_exec = str(pathlib.Path(f".venv/bin/{python_exec}"))
+        python_venv_exec = str(Path(f".venv/bin/{python_exec}"))
 
     os.system(f"{python_venv_exec} -m pip install -U pip setuptools wheel")
-    os.system(f"{python_venv_exec} -m pip install {editable} .{arg_extras}")
+    os.system(f"{python_venv_exec} -m pip install {editable} {path}{arg_extras}")
     os.system(f"{python_venv_exec} -m pip uninstall -y wheel")
