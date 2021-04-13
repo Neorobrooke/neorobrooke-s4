fc05f3ac4937c6edde07fa1cfb58fae2c17f2343
philippewarren <philippewarren31@gmail.com>
2021-04-13 16:57:15 -0400

    Déplacement des fichiers sur les démos dans un nouveau dossier

---
+++ Demos/script_demo_2.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from serial import Serial
+from funibot_api import Funibot, FuniArgs, FuniSerial
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
